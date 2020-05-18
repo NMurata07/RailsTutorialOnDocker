78518a303a63f5d4ae233ff44589d6461fdc74c7
Nobuharu Murata <mrtnbhr@gmail.com>
2020-05-18 21:09:09 +0900

    chapter-5-4-1

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
-  resources :microposts
-  resources :users
+  # resources :microposts
+  # resources :users
+  get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'users#index'
 end
--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -10,10 +10,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   #   assert_response :success
   # end
 
-  # test "should get new" do
-  #   get new_user_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get signup_path
+    assert_response :success
+  end
 
   # test "should create user" do
   #   assert_difference('User.count') do
