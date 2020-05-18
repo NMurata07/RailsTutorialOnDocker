34d235efe450003fcf8415b886b9faef28c71af7
Nobuharu Murata <mrtnbhr@gmail.com>
2020-05-18 13:43:22 +0900

    chapter-3-4-4

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  # root 'users#index'
 end
--- src/test/controllers/static_pages_controller_test.rb
+++ src/test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -19,6 +25,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
 
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
