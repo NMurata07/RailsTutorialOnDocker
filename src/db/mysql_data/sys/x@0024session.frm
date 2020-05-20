86b657e90c78be25f95b4b0739a7dbd84ce03e0b
Nobuharu Murata <mrtnbhr@gmail.com>
2020-05-20 21:37:01 +0900

    chapter-6-4

--- src/db/mysql_data/sys/x@0024session.frm
+++ src/db/mysql_data/sys/x@0024session.frm
@@ -7,7 +7,7 @@ definer_user=mysql.sys
 definer_host=localhost
 suid=0
 with_check_option=0
-timestamp=2020-05-12 06:01:17
+timestamp=2020-05-20 12:15:44
 create-version=1
 source=SELECT * FROM sys.x$processlist WHERE conn_id IS NOT NULL AND command != \'Daemon\'
 client_cs_name=utf8
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_163618) do
+ActiveRecord::Schema.define(version: 2020_05_19_165430) do
 
   create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_05_14_163618) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
